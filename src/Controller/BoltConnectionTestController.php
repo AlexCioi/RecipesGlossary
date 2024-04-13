f27b70e24f669bb1a86b163ab662f971eb435b86
Alex <alexandrucioi@gmail.com>
2024-04-13 20:01:53 +0300

    working bolt connection and authentication with user and password

---
+++ src/Controller/BoltConnectionTestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Bolt\Bolt;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BoltConnectionTestController extends AbstractController
+{
+    #[Route('/bolt', name: 'app_bolt_connection_test')]
+    public function index(): Response
+    {
+        $conn = new \Bolt\connection\Socket('neo4j://34.232.57.230', 7687);
+        $bolt = new \Bolt\Bolt($conn);
+
+        $bolt->setProtocolVersions(5.2);
+        $protocol = $bolt->build();
+
+        $response = $protocol->hello()->getResponse();
+        $response = $protocol->logon(['scheme' => 'basic',
+            'principal' => 'neo4j',
+            'credentials' => 'internship-2024'])
+            ->getResponse();
+
+        $protocol
+            ->run('MATCH p=()-->() RETURN p LIMIT 25')
+            ->pull();
+
+        foreach ($protocol->getResponses() as $response) {
+            dump($response);
+        }
+
+        return $this->render('bolt_connection_test/index.html.twig', [
+            'controller_name' => 'BoltConnectionTestController',
+        ]);
+    }
+}
